821b724327b6a5528e060645f1b8ac027e053ede
Laurine Ndanu Nzuki <nzukilaurine@gmail.com>
2024-07-10 03:08:37 +0300

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+
+    
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+}
+
+let namesArray = ['Guadelupe', 'Ollie', 'Aki'];
+let event = 'birthday';
+let messages = writeCards(namesArray, event);
+console.log(messages);
